a9fb13f11fdf91e9841500f677199ba1f336ad74
Liberontissauri <jp.liberon@gmail.com>
2021-07-22 23:12:33 +0100

    initial page of the app

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import styles from  '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleSection}>
+        <h1 className={"mainTitle"}>Quick Note</h1>
+        <h2 className={"text"}>Never forget what you want to remember</h2>
+      </div>
+      <div className={styles.buttonSection}>
+        
+      </div>
+    </div>
+  )
+}
